00cd8583bf9b231c98bcf4c97476f643a5513d46
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2019-12-17 10:42:55 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Simple python application!"
+    return "Simple python application by SMC!"
 
 if __name__ == "__main__":
     application.run()
