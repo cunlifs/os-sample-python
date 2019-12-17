79588656d9db86a46ea9514d4f605fb390252fa4
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2019-12-17 10:47:35 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
-from flask import Flask
-application = Flask(__name__)
+import pprint
 
-@application.route("/")
-def hello():
-    return "Simple python application by SMC!"
 
-if __name__ == "__main__":
-    application.run()
+def handler(environ, start_fn):
+    start_fn('200 OK', [('Content-Type', 'text/plain')])
+    return ["Hello World!\n"]
+
+
+def log_environ(handler):
+    def _inner(environ, start_fn):
+        pprint.pprint(environ)
+        return handler(environ, start_fn)
+    return _inner
+
+
+app = log_environ(handler)
